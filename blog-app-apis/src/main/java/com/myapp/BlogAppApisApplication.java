5d145eb374bcc50f597765278f8955d92534016b
Pratik <pratik.sonawane@valamis.com>
2023-05-30 07:07:12 +0530

    Global Exception Handle and VAlidator implemented

--- blog-app-apis/src/main/java/com/myapp/BlogAppApisApplication.java
+++ blog-app-apis/src/main/java/com/myapp/BlogAppApisApplication.java
@@ -1,8 +1,10 @@
 package com.myapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogAppApisApplication {
@@ -14,6 +16,11 @@ public class BlogAppApisApplication {
 		SpringApplication.run(BlogAppApisApplication.class, args);
 
 	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
 
 
 }
--- blog-app-apis/src/main/java/com/myapp/payload/UserDto.java
+++ blog-app-apis/src/main/java/com/myapp/payload/UserDto.java
@@ -4,14 +4,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserDto {
     private int id;
+   @NotEmpty
+   @Size(min=3, message = "Username must be min 3 chars")
     private String name;
+    @Email(message="Email address not found")
     private String email;
+    @NotEmpty
+    @Size(min = 3, max = 10, message = "Password must 3> <10")
     private String password;
+    @NotNull
     private String about;
 
 
